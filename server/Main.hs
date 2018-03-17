6859f7d8f4e4ea678b16b9601288c4a704e25f77
Joe Fiorini <joe@joefiorini.com>
2018-03-17 16:38:51 -0400

    Got some SQL working!

--- server/Main.hs
+++ server/Main.hs
@@ -23,8 +23,18 @@ import GraphQL
 import GraphQL.API
 import GraphQL.Resolver
 import GraphQL.Value.ToValue (ToValue(..))
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.ToField
+import Database.PostgreSQL.Simple.FromRow
+import Database.PostgreSQL.Simple.ToRow
 
 data Countdown = Countdown { short :: Text, description :: Text, startDate :: UTCTime, endDate :: UTCTime } deriving (Show,Generic)
+
+instance FromRow Countdown where
+  fromRow = Countdown <$> field <*> field <*> field <*> field
+
+instance ToRow Countdown where
+  toRow c = [toField $ short c, toField $ description c, toField $ startDate c, toField $ endDate c]
 
 type CountdownQuery = Object "Countdown" '[]
   '[Field "short" Text
@@ -34,9 +44,10 @@ type CountdownQuery = Object "Countdown" '[]
   ]
 
 type QueryRoot = Object "QueryRoot" '[]
-  '[Field "countdown" CountdownQuery]
-  -- '[ Argument "short" Text :> Field "countdown" Countdown
-  -- , Argument "description" Text :> Argument "short" (Maybe Text) :> Argument "startDate" String :> Argument "endDate" String :> Field "createCountdown" Countdown ]
+  -- '[Field "countdown" CountdownQuery]
+  -- '[ Argument "short" Text :> Field "countdown" (Maybe CountdownQuery) ]
+  '[ Argument "short" Text :> Field "countdown" (Maybe CountdownQuery)
+  , Argument "description" Text :> Argument "short" (Maybe Text) :> Argument "startDate" Text :> Argument "endDate" Text :> Field "createCountdown" CountdownQuery ]
 
 testCountdown = Countdown
   { short       = "blah"
@@ -45,27 +56,21 @@ testCountdown = Countdown
   , endDate     = UTCTime (fromGregorian 2018 3 23) 0
   }
 
-lookupCountdown :: Text -> IO (Maybe Countdown)
-lookupCountdown _ = pure $ Just Countdown
-  { short       = "blah"
-  , description = "Diddy doo dah"
-  , startDate   = UTCTime (fromGregorian 2018 3 16) 0
-  , endDate     = UTCTime (fromGregorian 2018 3 23) 0
-  }
--- lookupCountdown :: Text -> IO (Maybe Countdown)
--- lookupCountdown _ = do
---   startDate <- getCurrentTime
---   return $ Just Countdown
---     { short       = "blah"
---     , description = "Diddy doo dah"
---     , startDate   = startDate
---     , endDate     = getEndDate startDate
---     }
---   where getEndDate (UTCTime day time) = UTCTime (addDays 7 day) time
+lookupCountdown :: Connection -> Text -> IO (Maybe Countdown)
+lookupCountdown connection short = do
+  rows <-
+    query
+        connection
+        "select short, description, starts_on, ends_on from timers where short = ?"
+      $ Only short
+  return $ extractCountdown (rows :: [Countdown])
+ where
+  extractCountdown [countdown] = Just countdown
+  extractCountdown _           = Nothing
 
-countdownHandler :: Text -> Handler IO (Maybe CountdownQuery)
-countdownHandler short = do
-  countdown <- lookupCountdown short
+countdownHandler :: Connection -> Text -> Handler IO (Maybe CountdownQuery)
+countdownHandler conn short = do
+  countdown <- lookupCountdown conn short
   traverse (pure . viewCountdown) countdown
 
 viewCountdown :: Countdown -> Handler IO CountdownQuery
@@ -79,29 +84,72 @@ viewCountdown countdown@Countdown {..} =
   toStringTime =
     formatTime defaultTimeLocale (iso8601DateFormat $ Just "%H:%M:%S")
 
+parseUtcTime :: Text -> UTCTime
+parseUtcTime str = parseTimeOrError False
+                                    defaultTimeLocale
+                                    (iso8601DateFormat $ Just "%H:%M:%S")
+                                    (unpack str)
+
+extractCountdown :: [Countdown] -> Countdown
+extractCountdown [countdown] = countdown
+
 createCountdownHandler
-  :: Text -> Maybe Text -> String -> String -> Handler IO CountdownQuery
-createCountdownHandler description short startDate endDate = viewCountdown
-  Countdown
-    { short       = fromMaybe "blah" short
+  :: Connection
+  -> Text
+  -> Maybe Text
+  -> Text
+  -> Text
+  -> Handler IO CountdownQuery
+createCountdownHandler conn description short startDate endDate = do
+  rows <- query conn q buildCountdown
+  viewCountdown $ extractCountdown (rows :: [Countdown])
+ where
+  buildCountdown = Countdown
+    { short       = getOrMakeShort short
     , description = description
-    , startDate   = (read startDate) :: UTCTime
-    , endDate     = (read endDate) :: UTCTime
+    , startDate   = parseUtcTime startDate
+    , endDate     = parseUtcTime endDate
     }
+  getOrMakeShort (Just short) = short
+  getOrMakeShort Nothing      = "blah"
+  q
+    = "insert into timers (short, description, starts_on, ends_on) values (?,?,?,?) returning short, description, starts_on, ends_on"
 
+handler :: Connection -> Handler IO QueryRoot
+-- handler = pure (viewCountdown testCountdown)
+-- handler = pure countdownHandler
+handler conn = pure $ (countdownHandler conn) :<> (createCountdownHandler conn)
 
-handler :: Handler IO QueryRoot
-handler = pure (viewCountdown testCountdown)
--- handler = pure $ countdownHandler :<> createCountdownHandler
-
-runQuery :: Text -> IO Response
-runQuery = interpretAnonymousQuery @QueryRoot handler
+runQuery :: Connection -> Text -> IO Response
+runQuery conn = interpretAnonymousQuery @QueryRoot $ handler conn
 
 main :: IO ()
 main = scotty 3000 $ do
   middleware simpleCors
   post "/graphql" $ do
-    b        <- body
-    response <- liftAndCatchIO $ runQuery $ toQuery b
+    conn <- liftIO $ connectPostgreSQL "postgres://localhost/countdown-to_dev"
+    b <- body
+    response <- liftAndCatchIO $ runQuery conn $ toQuery b
     json $ toValue response
   where toQuery b = toStrict $ decodeUtf8 b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
